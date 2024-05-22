a6a28af6b48282b639f0930adfaeb540e0e8c287
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-05-22 18:02:02 +0530

    payment counter

--- src/Components/payment_Card.js
+++ src/Components/payment_Card.js
@@ -1,33 +1,51 @@
-import React from 'react'
-import { Button, Card} from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-
 export default function Payment_Card() {
-  return (
-    <Card  className='border_radias'>
-                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                        <Card.Body className='Payment_card'>
-                            <Card.Title>Order Summary</Card.Title>
-                            <Card.Text> Barcelona, Barcelona 08005 </Card.Text> <hr />
-                            <div className='d-flex justify-content-between'>
-                                <Card.Text> Amount</Card.Text> 
-                                <Card.Title>$19.00</Card.Title>
-                            </div>
-                            <div className='d-flex justify-content-between'>
-                                <Card.Text>Tax</Card.Text> 
-                                <Card.Title>$2.00</Card.Title>
-                            </div>
-                            
-                             <hr className='HRtage'/>
+    const [amount, setAmount] = useState(0);
 
-                            <div className='d-flex justify-content-between'>
-                                <Card.Title>Order Total</Card.Title> <br />
-                                <Card.Title>$21.00</Card.Title>
-                            </div> <br />
-                            <Button  className='w-100 Oder_coupun'><a href="#">Add coupon code here</a></Button>
-                            <Link to="/payment"><Button variant="primary" className='w-100 border_radias mt-4'>Checkout</Button></Link>
-                        </Card.Body>
-                </Card>
-  )
+    function handleChange(e) {
+        setAmount(parseFloat(e.target.value));
+    }
+
+    const tax = amount * 0.1;
+    const total = amount + tax;
+
+    return (
+        <Card className='border_radias'>
+            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+            <Card.Body className='Payment_card'>
+                <Card.Title>Order Summary</Card.Title>
+                <Card.Text> Barcelona, Barcelona 08005 </Card.Text>
+                <hr />
+
+                <div className='d-flex justify-content-between pb-2'>
+                    <h5 className='w-50'>Farm Rent</h5>
+                    <input type="number" className='form-control w-25' placeholder="000" value={amount} onChange={handleChange} />
+                </div>
+
+                <div className='d-flex justify-content-between'>
+                    <Card.Text> Amount</Card.Text>
+                    <Card.Title>${amount.toFixed(2)}</Card.Title>
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <Card.Text>Tax</Card.Text>
+                    <Card.Title>${tax.toFixed(2)}</Card.Title>
+                </div>
+
+                <hr className='HRtage'/>
+
+                <div className='d-flex justify-content-between'>
+                    <Card.Title>Order Total</Card.Title>
+                    <Card.Title>${total.toFixed(2)}</Card.Title>
+                </div>
+                <br />
+                <Button className='w-100 Oder_coupun'><a href="#">Add coupon code here</a></Button>
+                <Link to="/payment">
+                    <Button variant="primary" className='w-100 border_radias mt-4'>Checkout</Button>
+                </Link>
+            </Card.Body>
+        </Card>
+    )
 }
