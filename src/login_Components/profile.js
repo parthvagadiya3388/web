cca26ffb46e9fd71088213cb30d931552cda0a21
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-05-28 14:06:03 +0530

    changes last change 28

--- src/login_Components/profile.js
+++ src/login_Components/profile.js
@@ -27,7 +27,7 @@ export default function Profile() {
   const [lastName, setLastName] = useState('');
   const [address , setAddress] = useState();
   const [phone , setPhone] = useState();
-  // const [position , setPosition] = useState();
+  const [position , setPosition] = useState();
 
   useEffect(() => {
     setEmail(user.email);
